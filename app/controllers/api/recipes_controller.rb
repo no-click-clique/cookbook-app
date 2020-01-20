83780a795f694450ceb9be4ca7eded1b3324ec40
dani <dani.zaghian@gmail.com>
2020-01-20 09:55:30 -0800

    adds recipes index and show actions

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,8 +1,13 @@
 class Api::RecipesController < ApplicationController
 
-  def one_recipe_action
-    @recipe = Recipe.last #hash of recipe data
-    render "one_recipe.json.jb"
+  def index
+    @recipes = Recipe.all #array of recipe hashes
+    render "index.json.jb"
   end
 
+  def show
+    @recipe = Recipe.find_by(id: params["id"])
+    render "show.json.jb"
+  end
+  
 end
