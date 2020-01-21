5eebe575b7ef3c87de310979b209d71f6fb573c6
dani <dani.zaghian@gmail.com>
2020-01-21 09:56:00 -0800

    adds model methods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,30 @@
 class Recipe < ApplicationRecord
+  # attr_accessor :title, :ingredients, :directions, :prep_time
+
+  # # attr reader method
+  # def ingredients
+  #   @ingredients
+  # end
+
+  def ingredients_list
+    ingredients.split(", ")
+  end
+
+  def directions_list
+    directions.split(", ")
+  end
+
+  def friendly_created_at
+    created_at.strftime("%b %d, %Y")
+  end
+
+  def friendly_prep_time
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    result = ""
+    result += "#{hours} hours " if hours > 0
+    result += "#{minutes} minutes" if minutes > 0
+    result
+  end
+
 end
