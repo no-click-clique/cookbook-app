5fc71f760053e6a06d32556dd16c211ba238755c
dani <dani.zaghian@gmail.com>
2020-02-25 09:47:22 -0800

    adds custom partial syntax

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,15 +1,20 @@
 class Api::UsersController < ApplicationController
   def create
-    user = User.new(
+    @user = User.new(
       name: params[:name],
       email: params[:email],
       password: params[:password],
       password_confirmation: params[:password_confirmation]
     )
-    if user.save
-      render json: { message: "User created successfully" }, status: :created
+    if @user.save
+      render "show.json.jb"
     else
-      render json: { errors: user.errors.full_messages }, status: :bad_request
+      render json: { errors: @user.errors.full_messages }, status: :bad_request
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render "show.json.jb"
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,22 +3,21 @@ class ApplicationController < ActionController::Base
 
   # returns either a User hash from the database based on the JWT that got passed, or returns nil
   def current_user
-    User.first
-    # auth_headers = request.headers["Authorization"]
-    # if auth_headers.present? && auth_headers[/(?<=\A(Bearer ))\S+\z/]
-    #   token = auth_headers[/(?<=\A(Bearer ))\S+\z/]
-    #   begin
-    #     decoded_token = JWT.decode(
-    #       token,
-    #       Rails.application.credentials.fetch(:secret_key_base),
-    #       true,
-    #       { algorithm: "HS256" }
-    #     )
-    #     User.find_by(id: decoded_token[0]["user_id"])
-    #   rescue JWT::ExpiredSignature
-    #     nil
-    #   end
-    # end
+    auth_headers = request.headers["Authorization"]
+    if auth_headers.present? && auth_headers[/(?<=\A(Bearer ))\S+\z/]
+      token = auth_headers[/(?<=\A(Bearer ))\S+\z/]
+      begin
+        decoded_token = JWT.decode(
+          token,
+          Rails.application.credentials.fetch(:secret_key_base),
+          true,
+          { algorithm: "HS256" }
+        )
+        User.find_by(id: decoded_token[0]["user_id"])
+      rescue JWT::ExpiredSignature
+        nil
+      end
+    end
   end
 
   helper_method :current_user
