33f2c327420f0e3049fedd6118aeb86c498e7a5d
dani <dani.zaghian@gmail.com>
2020-03-05 11:45:43 -0800

    adds recipes update test and before do action

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Recipes", type: :request do
+
+  before do
+    user = User.create(name: "Dani", email: "dani@gmail.com", password: "password")
+    Recipe.create(title: "Example Title 1", ingredients: "Example Ingredients 1", directions: "Example Directions 1", image_url: "Example Image 1", prep_time: 30, user_id: user.id)
+    Recipe.create(title: "Example Title 2", ingredients: "Example Ingredients 2", directions: "Example Directions 2", image_url: "Example Image 2", prep_time: 30, user_id: user.id)
+    Recipe.create(title: "Example Title 3", ingredients: "Example Ingredients 3", directions: "Example Directions 3", image_url: "Example Image 3", prep_time: 30, user_id: user.id)
+  end
+
   describe "GET /recipes" do
     it "returns an array of recipe hashes" do
-      user = User.create(name: "Dani", email: "dani@gmail.com", password: "password")
-      Recipe.create(title: "Example Title 1", ingredients: "Example Ingredients 1", directions: "Example Directions 1", image_url: "Example Image 1", prep_time: 30, user_id: user.id)
-      Recipe.create(title: "Example Title 2", ingredients: "Example Ingredients 2", directions: "Example Directions 2", image_url: "Example Image 2", prep_time: 30, user_id: user.id)
-      Recipe.create(title: "Example Title 3", ingredients: "Example Ingredients 3", directions: "Example Directions 3", image_url: "Example Image 3", prep_time: 30, user_id: user.id)
       get "/api/recipes"
       expect(response).to have_http_status(200)
       recipes = JSON.parse(response.body)
@@ -16,9 +20,7 @@ RSpec.describe "Recipes", type: :request do
 
   describe "GET /recipes/:id" do
     it 'should return a hash with the appropriate recipe attributes' do
-      user = User.create(name: "Dani", email: "dani@gmail.com", password: "password")
-      recipe = Recipe.create(title: "Example Title 1", ingredients: "Example Ingredients 1", directions: "Example Directions 1", image_url: "Example Image 1", prep_time: 30, user_id: user.id)
-      get "/api/recipes/#{recipe.id}"
+      get "/api/recipes/#{Recipe.first.id}"
       recipe = JSON.parse(response.body)
       expect(response).to have_http_status(200)
       expect(recipe["title"]).to eq("Example Title 1")
@@ -28,8 +30,7 @@ RSpec.describe "Recipes", type: :request do
 
   describe "POST /recipes" do
     it 'should create a new recipe' do
-      user = User.create(name: "Dani", email: "dani@gmail.com", password: "password")
-      jwt = JWT.encode({user_id: user.id}, Rails.application.credentials.fetch(:secret_key_base), "HS256")
+      jwt = JWT.encode({user_id: User.first.id}, Rails.application.credentials.fetch(:secret_key_base), "HS256")
       post "/api/recipes", params: {
         title: "New Recipe",
         ingredients: "New Ingredients",
@@ -42,14 +43,42 @@ RSpec.describe "Recipes", type: :request do
       expect(recipe["title"]).to eq("New Recipe")
     end
     it 'should return an error status code with invalid data' do
-      user = User.create(name: "Dani", email: "dani@gmail.com", password: "password")
-      jwt = JWT.encode({user_id: user.id}, Rails.application.credentials.fetch(:secret_key_base), "HS256")
+      jwt = JWT.encode({user_id: User.first.id}, Rails.application.credentials.fetch(:secret_key_base), "HS256")
       post "/api/recipes", params: {}, headers: { "Authorization" => "Bearer #{jwt}" }
       recipe = JSON.parse(response.body)
       expect(response).to have_http_status(:unprocessable_entity)
     end
     it 'should return an error status code with no jwt' do
       post "/api/recipes", params: {}
+      recipe = JSON.parse(response.body)
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
+
+  describe "PATCH /recipes/:id" do
+    it 'should update a recipe' do
+      jwt = JWT.encode({user_id: User.first.id}, Rails.application.credentials.fetch(:secret_key_base), "HS256")
+
+      patch "/api/recipes/#{Recipe.first.id}", 
+      params: { title: "New Title" },
+      headers: { "Authorization" => "Bearer #{jwt}" }
+
+      recipe = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(recipe["title"]).to eq("New Title")
+    end
+    it 'should return 422 with bad data' do
+      jwt = JWT.encode({user_id: User.first.id}, Rails.application.credentials.fetch(:secret_key_base), "HS256")
+
+      patch "/api/recipes/#{Recipe.first.id}", 
+      params: { title: "" },
+      headers: { "Authorization" => "Bearer #{jwt}" }
+
+      recipe = JSON.parse(response.body)
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+    it 'should return an error status code with no jwt' do
+      patch "/api/recipes/#{Recipe.first.id}", params: {}
       recipe = JSON.parse(response.body)
       expect(response).to have_http_status(:unauthorized)
     end
