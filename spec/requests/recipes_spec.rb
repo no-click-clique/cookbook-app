4805db9658514c8be5e81d4be9eb3b38f2b078be
dani <dani.zaghian@gmail.com>
2020-03-05 09:31:33 -0800

    adds rspec and recipes index test

---
+++ spec/requests/recipes_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Recipes", type: :request do
+  describe "GET /recipes" do
+    it "returns an array of recipe hashes" do
+      user = User.create(name: "Dani", email: "dani@gmail.com", password: "password")
+      Recipe.create(title: "Example Title 1", ingredients: "Example Ingredients 1", directions: "Example Directions 1", image_url: "Example Image 1", prep_time: 30, user_id: user.id)
+      Recipe.create(title: "Example Title 2", ingredients: "Example Ingredients 2", directions: "Example Directions 2", image_url: "Example Image 2", prep_time: 30, user_id: user.id)
+      Recipe.create(title: "Example Title 3", ingredients: "Example Ingredients 3", directions: "Example Directions 3", image_url: "Example Image 3", prep_time: 30, user_id: user.id)
+      get "/api/recipes"
+      expect(response).to have_http_status(200)
+      recipes = JSON.parse(response.body)
+      expect(recipes.length).to eq(3)
+    end
+  end
+end
